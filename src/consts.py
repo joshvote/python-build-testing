ab9b121ac35c173a8b2f7f0faaad2ad172206f1a
Josh Vote <jjvote@gmail.com>
2022-10-07 16:29:49 +0800

    Adding security risk

--- src/consts.py
+++ src/consts.py
@@ -3,3 +3,10 @@ MEDIUM_STRING = "this is a medium string this is a medium string this is a mediu
 SHORT_STRING = "this is a short string"
 ANOTHER_STRING = "another"
 
+# Adding a security flaw for bandit to find
+def securityRisk():
+    import subprocess
+
+    domain = input("Enter the Domain: ")
+    output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding="UTF-8")
+    print(output)
