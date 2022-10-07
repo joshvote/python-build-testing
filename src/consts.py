9b525139ec099591c93e74d5e269b75e7672861c
Josh Vote <jjvote@gmail.com>
2022-10-07 16:22:58 +0800

    Added new string to mark file as edited

--- src/consts.py
+++ src/consts.py
@@ -1,4 +1,5 @@
-LONG_STRING = 'this is a really long string this is a really long string this is a really long string this is a really long string this is a really long string '
-MEDIUM_STRING = 'this is a medium string this is a medium string this is a medium string this is a medium string '
-SHORT_STRING = 'this is a short string'
+LONG_STRING = "this is a really long string this is a really long string this is a really long string this is a really long string this is a really long string "
+MEDIUM_STRING = "this is a medium string this is a medium string this is a medium string this is a medium string "
+SHORT_STRING = "this is a short string"
+ANOTHER_STRING = "another"
 
